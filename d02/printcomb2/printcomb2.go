aa156e4b58a4d510099dfa7351009042946bf470
Olive Frédéric <33631285+Ofdev12@users.noreply.github.com>
2018-10-23 23:11:24 +0200

    Create printcomb2.go

---
+++ d02/printcomb2/printcomb2.go
@@ -0,0 +1,30 @@
+package main
+
+import "github.com/01-edu/O1"
+
+func show(a int, b int) {
+	if a < b {
+		O1.Putchar(byte(a / 10 + 48))
+		O1.Putchar(byte(a % 10 + 48))
+		O1.Putchar(' ')
+		O1.Putchar(byte(b / 10 + 48))
+		O1.Putchar(byte(b % 10 + 48))
+		if !(a == 98 && b == 99) {
+			O1.Putchar(',')
+			O1.Putchar(' ')
+		}
+	}
+}
+
+func printComb2() {
+	a := int(0)
+	b := int(0)
+	for a <= 98 {
+		b = a + 1
+		for b <= 99 {
+			show(a, b)
+			b++
+		}
+		a++
+	}
+}
