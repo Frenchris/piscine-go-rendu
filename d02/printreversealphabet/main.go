634a6dfdb656125d8c6e44d810f821ce6a3fab07
Olive Frédéric <33631285+Ofdev12@users.noreply.github.com>
2018-10-23 17:42:05 +0200

    Update main.go

--- d02/printreversealphabet/main.go
+++ d02/printreversealphabet/main.go
@@ -3,13 +3,13 @@ package main
 import "github.com/01-edu/O1"
 
 func printReverseAlphabet() {
-  i := byte('z')
-  for i >= 'a'{
-    putchar(i)
-    i--
-  }
+	i := byte('z')
+	for i >= 'a'{
+		putchar(i)
+		i--
+	}
 }
 
 func main() {
-  printReverseAlphabet()
+	printReverseAlphabet()
 }
